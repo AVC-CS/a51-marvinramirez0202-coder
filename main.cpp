77eec310610e4667553e734084ddecaf77137322
marvinramirez0202-coder <marvinramirez0202@gmail.com>
2026-03-23 12:44:08 -0700

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -7,11 +7,26 @@ using namespace std;
 int main()
 {
     // TODO: declare variable n for the exponent input
-
+    double n;
+    double result = 1.0;
     // TODO: read n from standard input
+    cin >> n;
 
     // TODO: compute 2 to the power of n using a loop (no cmath pow)
     // For positive n: multiply result by 2, n times
     // For negative n: divide result by 2, |n| times
 
-}
+    if (n >= 0){
+        for (int i = 0; i <= n; i++){
+            result *= 2;
+        }
+        cout << result << endl;
+    }
+    else{
+        n = n * -1;
+        for(int i = 0; i <= n; i++){
+            result *= 2;
+        }
+        cout << 1.0 / result << endl;
+    }
+}   
